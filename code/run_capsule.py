979b3ae8e457a49cd085d4033393cd1a0aebdd5c
Ahad Bawany <ahad.bawany@alleninstitute.org>
2024-06-25 19:40:32 +0000

    Edited run_capsule.py

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -188,7 +188,7 @@ def run():
 
         dx_deg = utils.running_from_stim_file(stim_file, "dx", num_raw_timestamps)
         if len(dx_deg) > num_raw_timestamps:
-            dx_deg = dx_deg[:num_raw_timestamps]
+            num_raw_timestamps = len(dx_deg)
 
         if num_raw_timestamps != len(dx_deg):
             raise ValueError(
